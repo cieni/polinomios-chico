5aabbdb1ffed9deac9e699217053fd71ce71167b
IagoRampazzo <iagorampazzo@hotmail.com>
2014-04-16 11:21:32 -0300

    Polinomios
    Feito Multiplicação por outro Polinômio("A*B") + ("A-B").

---
+++ Iago/PolinomioComoListaSimples.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Polinomio
+{
+    class PolinomioComoListaSimples : Polinomio
+    {
+        ListaSimples<Termo> lista;
+
+        public PolinomioComoListaSimples()
+        {
+            lista = new ListaSimples<Termo>();
+        }
+
+        public override void Incluir(Termo termo) // incluirá Termo na lista, em ordem decrescente de expoente
+        {
+            ListaSimples<Termo>.NoLista noAtu = lista.Primeiro;
+            int acabou = 0; // acabou ficará 1 quando já tiver incluído o novo termo da lista
+            while (noAtu != null && acabou < 1)
+            {
+                if (termo.CompareTo(noAtu) > 0) // termo.expoente > noAtu.expoente
+                {
+                    noAtu.InsereAntes(termo); // como é em ordem decrescente, deverá ser incluído antes do 1º termo menor que ele
+                    acabou = 1;
+                }
+                noAtu = noAtu.prox;
+            }
+        }
+
+        public override void Diferenciar() // percorrerá os termos e os diferenciará
+        {
+            ListaSimples<Termo>.NoLista noAtu = lista.Primeiro;
+            while (noAtu != null)
+            {                               //diferencia os termos de cada nó da ListaSimples
+                noAtu.dados.Diferenciar();
+                noAtu = noAtu.prox;
+            }
+        }
+
+        public override Polinomio SomadoA(Polinomio p) // devolve novo polinômio (this + p)
+        {
+            return null;
+        }
+
+        public override Polinomio MultiplicarPorPolinomio(PolinomioComoListaSimples p)
+        {
+            PolinomioComoListaSimples resultado = new PolinomioComoListaSimples();
+            ListaSimples<Termo>.NoLista noAtu = resultado.lista.Primeiro;
+            ListaSimples<Termo>.NoLista noDele = p.lista.Primeiro;
+
+            while (noAtu != null)
+            {
+                while (noDele != null)
+                {
+                    int exp;
+                    double coef;
+                    coef = noAtu.dados.Coeficiente * noDele.dados.Coeficiente;
+                    exp  = noAtu.dados.Expoente + noDele.dados.Expoente;
+                    resultado.Incluir(new Termo(coef,exp));
+                    noDele = noDele.prox;
+                }
+                noAtu = noAtu.prox;
+            }
+
+            return resultado;
+        }
+
+
+    }
+
+}
+
---
+++ Iago/Principal.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Polinomio
+{
+    public partial class Principal : Form
+    {
+        Polinomio polA,polB;
+        public Principal()
+        {
+            InitializeComponent();
+            polA = new PolinomioComoListaSimples();
+            polB = new PolinomioComoListaSimples();
+        }
+
+        private Polinomio lerTermoDeArquivo()
+        {
+            PolinomioComoListaSimples lido = new PolinomioComoListaSimples();
+            if (ofdAbrir.ShowDialog() == DialogResult.OK)
+            {
+                if (ofdAbrir.OpenFile() != null)
+                {
+                    int indAtu = 0,exp;
+                    double coef;
+                    String caminho = ofdAbrir.FileName;
+                    StreamReader sr = new StreamReader(caminho);
+                    string[] linha;
+                    linha = sr.ReadToEnd().Split(' ');   //le palavra por palavra separadas por espaço
+                    sr.Close();
+                    while (linha[indAtu] != null)
+                    {
+                        coef = Convert.ToInt32(linha[indAtu]);
+                        if (linha[indAtu + 1] != null)
+                            exp = Convert.ToInt32(linha[indAtu + 1]);
+                        else
+                            exp = 1;
+                        Termo atual = new Termo(coef, exp);
+                        lido.Incluir(atual);
+                        indAtu += 2;
+                    }                   
+                }
+            }
+            return lido;
+        }
+
+        private void button11_Click(object sender, EventArgs e) //ler de arquivo
+        {
+           polA = lerTermoDeArquivo();
+           label6.Text = "A: " + polA;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            polB = lerTermoDeArquivo();
+            label7.Text = "B: " + polB;
+        }
+
+        private void Principal_Load(object sender, EventArgs e)
+        {
+            ofdAbrir = new OpenFileDialog();
+            ofdAbrir.Filter = "Text files (*.txt)|*.txt";       // só poderá abrir arquivo de extensão txt
+            ofdAbrir.RestoreDirectory = true;            //faz com que ele sempre abra onde foi selecionado ante
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int exp;
+            double coef;
+            coef = Convert.ToInt32(coefA.Text);
+            exp = Convert.ToInt32(expA.Text);
+            Termo novoTermo;
+            novoTermo = new Termo(coef,exp);
+            polA.Incluir(novoTermo);
+            label6.Text = "A: "+polA.ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int exp;
+            double coef;
+            coef = Convert.ToInt32(coefA.Text);
+            exp = Convert.ToInt32(expA.Text);
+            Termo novoTermo;
+            novoTermo = new Termo(coef, exp);
+            polB.Incluir(novoTermo);
+            label7.Text = "B: "+polB.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Polinomio polResultado;
+            polResultado = polA.SomadoA(polB);
+            txtResultado.Text = polResultado.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Polinomio polResultado;
+            polResultado = polB.MultiplicarPorConstante(-1);
+            polResultado.SomadoA(polA);            
+            txtResultado.Text = polResultado.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Polinomio polResultado;
+            polResultado = polA.MultiplicarPorConstante(-1);
+            polResultado.SomadoA(polB);
+            txtResultado.Text = polResultado.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Polinomio polResultado = polA.MultiplicarPorPolinomio(polB);
+            txtResultado.Text = polResultado.ToString();
+        }
+
+    }
+}
